85ec8d154f824229bdb53f74740dbe621ccc690d
tugalsan karabacak <tugalsan@gmail.com>
2022-07-05 17:04:28 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/tst/automodule/child/client/TGS_TstAutomoduleChild.java
@@ -0,0 +1,8 @@
+package com.tugalsan.tst.automodule.child.client;
+
+public class TGS_TstAutomoduleChild {
+
+    public static void doSth() {
+        System.out.println("ali gel old");
+    }
+}
---
+++ src/main/java/com/tugalsan/tst/automodule/parent2/client/TGS_TstAutomoduleParent2.java
@@ -0,0 +1,10 @@
+package com.tugalsan.tst.automodule.parent2.client;
+
+import com.tugalsan.tst.automodule.child.client.*;
+
+public class TGS_TstAutomoduleParent2 {
+
+    public static void doSth() {
+        TGS_TstAutomoduleChild.doSth();
+    }
+}
